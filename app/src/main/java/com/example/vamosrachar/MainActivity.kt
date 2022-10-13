2383f08bd70edefef65bf8fb6768995ae097aa8a
Guilhermeslv <guilhermeslv78@gmail.com>
2022-10-12 23:23:51 -0300

    Modificações no cálculo e exibição

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.vamosrachar
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.DecimalFormat
+import java.text.NumberFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,41 +21,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Elementos da tela
         val quantidade_pessoas: TextView = findViewById(R.id.editTextNumberSigned)
         val valor_total: TextView = findViewById(R.id.editTextNumberDecimal)
         val valor_exibido: TextView = findViewById(R.id.valorExibido)
         val btn_tts: FloatingActionButton = findViewById(R.id.floatingActionButton2)
         val btn_share: FloatingActionButton = findViewById(R.id.floatingActionButton)
 
+        //Text-To-Speech
         mtts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
             if (status != TextToSpeech.ERROR){
                 mtts.language = Locale.UK
             }
         })
 
+        //Conversão para Float
         var valor_total_float = valor_total.text.toString().toFloatOrNull()
         var quantidade_pessoas_float = quantidade_pessoas.text.toString().toFloatOrNull()
 
         //Observador de mudanças no texto
-        quantidade_pessoas.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                quantidade_pessoas_float = quantidade_pessoas.text.toString().toFloatOrNull()
-                Log.v("PDM", "Quantidade: " + quantidade_pessoas_float + " Valor: " + valor_total_float)
-
-                if (valor_total_float == null|| quantidade_pessoas_float == null){
-                    valor_exibido.text = "Calculando..."
-                }else{
-                    val calculo = valor_total_float!! / quantidade_pessoas_float!!
-                    valor_exibido.text = "R$ " + calculo
-                }
-            }
-        });
         valor_total.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
@@ -63,18 +49,39 @@ class MainActivity : AppCompatActivity() {
 
             override fun afterTextChanged(p0: Editable?) {
                 valor_total_float = valor_total.text.toString().toFloatOrNull()
-                Log.v("PDM", "Quantidade: " + quantidade_pessoas_float + " Valor: " + valor_total_float)
 
-                if (valor_total_float == null|| quantidade_pessoas_float == null){
-                    valor_exibido.text = "Calculando..."
+                if (valor_total_float == null || quantidade_pessoas_float == null || quantidade_pessoas_float == 0f){
+                    valor_exibido.text = "Insira os valores"
                 }else{
                     val calculo = valor_total_float!! / quantidade_pessoas_float!!
-                    valor_exibido.text = "R$ " + calculo
+                    val formatter: NumberFormat = DecimalFormat("0.##")
+                    Log.v("PDM", formatter.format(calculo))
+                    valor_exibido.text = "R$ " + formatter.format(calculo)
                 }
             }
         });
+        quantidade_pessoas.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
-        //Observador de botão
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                quantidade_pessoas_float = quantidade_pessoas.text.toString().toFloatOrNull()
+
+                if (valor_total_float == null || quantidade_pessoas_float == null || quantidade_pessoas_float == 0f){
+                    valor_exibido.text = "Insira os valores"
+                }else{
+                    val calculo = valor_total_float!! / quantidade_pessoas_float!!
+                    val formatter: NumberFormat = DecimalFormat("0.##")
+                    valor_exibido.text = "R$ " + formatter.format(calculo)
+                }
+
+            }
+        });
+
+        //Botão Text-To-Speech
         btn_tts.setOnClickListener{
             val toSpeak = valor_exibido.text.toString()
             if (toSpeak == "Calculando..." || toSpeak == "Insira os valores"){
@@ -83,6 +90,8 @@ class MainActivity : AppCompatActivity() {
                 mtts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
             }
         }
+
+        //Botão de compartilhar
         btn_share.setOnClickListener{
             val texto_comp = "O valor para cada amigo ficou: "+ valor_exibido.text.toString() +" - Vamos Rachar!"
             val shareIntent = Intent()
@@ -92,4 +101,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent.createChooser(shareIntent,"Compartilhar"))
         }
     }
+
 }
